450dc77687042793c2a2a2c849fca9820a910f54
TAKAMURAFUMIHITO <108682238+TAKAMURAFUMIHITO@users.noreply.github.com>
2023-01-21 19:27:10 +0900

    保存機能を追加

--- index.js
+++ index.js
@@ -1,19 +1,41 @@
 const form = document.getElementById("form");
 const input = document.getElementById("input");
 const ul = document.getElementById("ul");
+const toDos = JSON.parse(localStorage.getItem("toDos"));
+
+if (toDos) {
+  toDos.forEach(toDo => {
+    add(toDo);
+  })
+}
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
   add();
 });
 
-function add() {
+function add(toDo) {
   let toDoText = input.value;
+
+  if (toDo) {
+    toDoText = toDo;
+  }
+
   if (toDoText) {
     const li = document.createElement("li");
     li.innerText = toDoText;
     li.classList.add("list-group-item")
-    ul.appendChild("li");
+    ul.appendChild(li);
     input.value = "";
+    saveData();
   }
+}
+
+function saveData() {
+  const lists = document.querySelectorAll("li");
+  let toDos = [];
+  lists.forEach(list => {
+    toDos.push(list.innerText);
+  });
+  localStorage.setItem("toDos", JSON.stringify(toDos));
 }
